c42712a726ea2adcd95c7e332bda8480fdf27637
stars22 <maildeal@gmail.com>
2020-03-24 19:46:34 +0300

    feat: parse cli args implementation

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const { program } = require("commander");
+const { pipeline } = require("stream");
+const fs = require("fs");
 const caesarCipher = require("./caesarCipher");
+
+program
+  .requiredOption("-s, --shift <number>", "a shift")
+  .option("-i, --input <filename>", "an input file")
+  .option("-o, --output", "an output file")
+  .requiredOption("-a, --action", "an action encode/decode");
+
+program.parse(process.argv);
